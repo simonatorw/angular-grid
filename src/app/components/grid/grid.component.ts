335198a801fe7c1521541a87fb307a7cebf607b0
bigredtiger <simon@bigredtiger.com>
2018-01-31 21:56:07 -0500

    Added css grid

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'grid',
   templateUrl: './grid.component.html',
-  //styleUrls: ['./app.component.css']
+  styleUrls: ['./grid.component.css']
 })
 export class GridComponent {
 }
